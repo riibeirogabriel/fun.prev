0957ddfceeae27141b67e86e42849721cc6d00a7
Gabriel Ribeiro <gabbriel.rribeiro@gmail.com>
2020-05-30 18:16:15 -0300

    Creating models and quiz data

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+import json
+from django.conf import settings
+import os
 
-# Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+field_length = 500
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=field_length)
+    city = models.CharField(max_length=field_length)
+    years_old = models.IntegerField()
+
+
+class Quiz(models.Model):
+    ask = models.CharField(max_length=field_length)
+    answer_one = models.CharField(max_length=field_length)
+    answer_two = models.CharField(max_length=field_length)
+    answer_three = models.CharField(max_length=field_length)
+    answer_four = models.CharField(max_length=field_length)
+    true_answer = models.IntegerField()
+
--- hello/views.py
+++ hello/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Quiz
+import json
+import os
+from django.conf import settings
 
-from .models import Greeting
 
-# Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, "home.html")
 
 
-def db(request):
+def quiz_list(request):
+    with open(os.path.join(settings.BASE_DIR, 'quiz.json'),
+              encoding='utf-8') as file:
+        data = json.load(file)
 
-    greeting = Greeting()
-    greeting.save()
+        for question in data["asks"]:
 
-    greetings = Greeting.objects.all()
+            Quiz.objects.create(
+                ask=question["ask"],
+                answer_one=question["answer_one"],
+                answer_two=question["answer_two"],
+                answer_three=question["answer_three"],
+                answer_four=question["answer_four"],
+                true_answer=question["true_answer"]
+                )
 
-    return render(request, "db.html", {"greetings": greetings})
+    quiz = Quiz.objects.all()
+    return render(request, "quiz.html", {"quiz": quiz})
